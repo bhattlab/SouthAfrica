52a396ffd0a959dfcc893df1ae866eff0f413713
tamburif <tamburif@gene.com>
2021-11-18 23:04:22 -0800

    final table scripts

--- scripts/tables/table1_participant_summary.R
+++ scripts/tables/table1_participant_summary.R
@@ -6,31 +6,31 @@ library(tidyverse)
 load(here("RData/metadata.RData"))
 
 summary_tbl <- za_pheno %>%
-  mutate(age_mb = age) %>%
+  # mutate(mb_age = age) %>%
   group_by(sample) %>%
-  mutate(
-    bp_sys = mean(c(bp_sys_1, bp_sys_2, bp_sys_3), na.rm = T),
-    bp_dia = mean(c(bp_dia_1, bp_dia_2, bp_dia_3), na.rm = T),
-  ) %>%
+  # mutate(
+  #   bp_sys = mean(c(bp_sys_1, bp_sys_2, bp_sys_3), na.rm = T),
+  #   bp_dia = mean(c(bp_dia_1, bp_dia_2, bp_dia_3), na.rm = T),
+  # ) %>%
   group_by(site) %>%
   summarise(
     n_participants = n(),
-    age_mean = mean(age_mb),
-    age_sd = sd(age_mb),
-    age_low = min(age_mb),
-    age_high = max(age_mb),
-    bmi_mean = mean(bmi, na.rm = T),
-    bmi_sd = sd(bmi, na.rm = T),
-    bmi_low = min(bmi, na.rm = T),
-    bmi_high = max(bmi, na.rm = T),
-    bp.sys_mean = mean(bp_sys, na.rm = T),
-    bp.sys_sd = sd(bp_sys, na.rm = T),
-    bp.sys_low = min(bp_sys, na.rm = T),
-    bp.sys_high = max(bp_sys, na.rm = T),
-    bp.dia_mean = mean(bp_dia, na.rm = T),
-    bp.dia_sd = sd(bp_dia, na.rm = T),
-    bp.dia_low = min(bp_dia, na.rm = T),
-    bp.dia_high = max(bp_dia, na.rm = T)
+    age_mean = mean(mb_age),
+    age_sd = sd(mb_age),
+    age_low = min(mb_age),
+    age_high = max(mb_age),
+    bmi_mean = mean(bmi_values, na.rm = T),
+    bmi_sd = sd(bmi_values, na.rm = T),
+    bmi_low = min(bmi_values, na.rm = T),
+    bmi_high = max(bmi_values, na.rm = T),
+    bp.sys_mean = mean(avg_sys_bp, na.rm = T),
+    bp.sys_sd = sd(avg_sys_bp, na.rm = T),
+    bp.sys_low = min(avg_sys_bp, na.rm = T),
+    bp.sys_high = max(avg_sys_bp, na.rm = T),
+    bp.dia_mean = mean(avg_dia_bp, na.rm = T),
+    bp.dia_sd = sd(avg_dia_bp, na.rm = T),
+    bp.dia_low = min(avg_dia_bp, na.rm = T),
+    bp.dia_high = max(avg_dia_bp, na.rm = T)
   ) %>%
   mutate(across(where(is.numeric), ~ round(., 2)))
 
